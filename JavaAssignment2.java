8dfd32f4b793d377a92b978a7c4c55c532b63ec6
Narsingani Amisha <33957781+ami9901@users.noreply.github.com>
2019-02-19 13:09:52 +0530

    bank database jdbc servlet connection

---
+++ JavaAssignment2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment2;
+
+/**
+ *
+ * @author Ruchitesh
+ */
+public class JavaAssignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BankForm form = new BankForm();
+        form.setVisible(true);
+    }
+    
+}
